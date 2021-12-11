304f7fe59da7baf8a198a08068c976a09b0cbeb3
Granite Bagas <granitebagas28@gmail.com>
2021-12-11 10:18:48 +0700

    Add response helper function

---
+++ app/Helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\JsonResponse;
+
+class Helpers
+{
+    public static function successResponse(
+        string $message = '',
+        array $result = [],
+        int $code = 200
+    ): JsonResponse {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $result,
+        ], $code);
+    }
+
+    public static function errorResponse(
+        string $message = '',
+        int $code = 400
+    ): JsonResponse {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+        ], $code);
+    }
+}
