d55b3f4c2acf2435344dbe9faa9f88701ca14429
Granite Bagas <granitebagas28@gmail.com>
2021-12-11 10:47:48 +0700

    Fix users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('password');
-            $table->unsignedDouble('unit');
-            $table->unsignedDouble('balance');
+            $table->unsignedDouble('unit')->default(0);
+            $table->unsignedDouble('balance')->default(0);
             $table->timestamps();
         });
     }
